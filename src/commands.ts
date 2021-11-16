c3ad1513bd61a8f64c401888c7e154276d6e6ee7
Jan Richter <jrichter@redhat.com>
2021-11-16 09:21:03 +0100

    Break code into methods

---
+++ src/commands.ts
@@ -0,0 +1,5 @@
+import { window } from 'vscode';
+
+export const helloWorld = () => {
+    return window.showInformationMessage('Hello World!');
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { helloWorld } from './commands'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -16,7 +17,7 @@ export function activate(context: vscode.ExtensionContext) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		helloWorld();
 	});
 
 	context.subscriptions.push(disposable);
