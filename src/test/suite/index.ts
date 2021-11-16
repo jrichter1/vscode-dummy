562f9a2598e88ef5af88f1da0ba147c3f5675ccd
Jan Richter <jrichter@redhat.com>
2021-11-16 11:56:09 +0100

    Add test coverage

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,15 +1,39 @@
 import * as path from 'path';
 import * as Mocha from 'mocha';
 import * as glob from 'glob';
+const NYC = require('nyc');
 
-export function run(): Promise<void> {
+async function setupCoverage() {
+    const nyc = new NYC(
+        {// set the project root
+            cwd: path.join(__dirname, '..', '..', '..'),
+            include: ['src/**/*.ts', 'out/**/*.js'],
+            exclude: ['**/*test/**', '.vscode-test/**'],
+            reporter: ['text', 'text-lcov', 'lcov', 'json'],
+            tempDir: path.join(__dirname, '..', '..', '..', '.nyc_output'),
+            all: true,
+            checkCoverage: true,
+            instrument: true,
+            hookRequire: true,
+            hookRunInContext: true,
+            hookRunInThisContext: true,
+        });
+
+    await nyc.reset();
+    await nyc.wrap();
+
+    return nyc;
+}
+
+export async function run(): Promise<void> {
 	// Create the mocha test
 	const mocha = new Mocha();
 
+	const nyc = await setupCoverage();
 	const testsRoot = path.resolve(__dirname);
 
 	return new Promise((c, e) => {
-		glob('**/**-test.js', { cwd: testsRoot }, (err, files) => {
+		glob('**/**-test.js', { cwd: testsRoot }, async (err, files) => {
 			if (err) {
 				return e(err);
 			}
@@ -22,13 +46,17 @@ export function run(): Promise<void> {
 				mocha.run(failures => {
 					if (failures > 0) {
 						e(new Error(`${failures} tests failed.`));
-					} else {
-						c();
 					}
 				});
 			} catch (err) {
 				console.error(err);
 				e(err);
+			} finally {
+				if (nyc) {
+					await nyc.writeCoverageFile();
+					await nyc.report();
+					c();
+				}
 			}
 		});
 	});
