a7a6770990e7f888d82716e24f796ba6dea15adf
Jan Richter <jrichter@redhat.com>
2021-11-16 11:17:14 +0100

    Stubs introduction

--- src/test/suite/commands-test.ts
+++ src/test/suite/commands-test.ts
@@ -1,13 +1,27 @@
 import assert = require('assert');
 import { helloWorld } from '../../commands';
+import { window } from 'vscode';
+import * as sinon from 'sinon';
 
 describe('HelloWorld command', () => {
-    
+    let sandbox: sinon.SinonSandbox;
+
+    before(() => {
+        sandbox = sinon.createSandbox();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
     it('HelloWorld command runs without error', async () => {
+        const stub = sandbox.stub(window, 'showInformationMessage').resolves();
         try {
             await helloWorld();
         } catch (err) {
             assert.fail(err as Error);
+        } finally {
+            assert(stub.callCount === 1);
         }
     });
 });
